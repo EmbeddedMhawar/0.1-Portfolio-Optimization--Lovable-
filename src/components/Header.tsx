72bae80588a818b811789964203596acadb82cae
EmbeddedMhawar <abderrahmanemhawar@gmail.com>
2025-06-01 13:06:48 +0100

    Deploy application to Netlify

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,10 +15,12 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-between px-10 py-3 border-b border-[#dee2e6] dark:border-[#2e4328] bg-white dark:bg-[#162013] transition-colors duration-700">
       <div className="flex items-center gap-4">
-        <div className="w-8 h-8 relative">
-          <div className="absolute inset-0 bg-[#2e4328] dark:bg-white" style={{ clipPath: 'polygon(0 0, 100% 0, 100% 50%, 50% 50%, 50% 100%, 0 100%)' }}></div>
+        <div className="w-4 h-4">
+          <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z" fill="currentColor" className="text-[#343a40] dark:text-white" />
+          </svg>
         </div>
-        <h1 className="text-[#343a40] dark:text-white text-xl font-bold">Invsmart</h1>
+        <h1 className="text-[#343a40] dark:text-white text-lg font-bold">InvestSmart</h1>
       </div>
 
       <button 
